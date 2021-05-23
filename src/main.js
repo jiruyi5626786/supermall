32e3e394f569f3b1d12cc9aed3c7b89fece896a3
jiruyi5626786 <406502870@qq.com>
2021-05-23 17:12:42 +0800

    '初始化项目'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
+Vue.config.productionTip = false
+// createApp(App).mount('#app')
 createApp(App).mount('#app')
+new Vue({
+  el:'#app',
+  router
+})
+
+// new Vue({
+//   render: h => h(App),
+//   router
+// }).$mount('#app')
+
